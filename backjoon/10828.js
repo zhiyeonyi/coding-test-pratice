e89bd0774c02e12c340fba24d00c831c143ee35c
zhiyeonyi <yjy39k@naver.com>
2022-04-01 14:08:24 +0900

    미완료

--- backjoon/10828.js
+++ backjoon/10828.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const input = fs.readFileSync('example.txt').toString().trim().split('/n');
+const input = fs.readFileSync('example.txt').toString().trim().split('\n');
 
 const N = +input.shift();
 
---
+++ backjoon/11279.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const input = fs.readFileSync('example.txt').toString().trim().split('\n');
+
+const n=input.shift();
+const answer = []; //담는 배열
+
+for(let i=0; i<input.length; i++){
+    const num = Number(input[i]);
+
+    var max = Math.max(...answer);
+    console.log(max)
+    if(num ===0 ){
+        answer.length === 0 ? console.log(0) :
+        console.log(max) && answer.filter((e)=> e !== max) 
+    } else if(num%1===0){
+        answer.push(num)
+    }
+    // num===0 ? console.log(max) && input.filter((e) => e == max) : answer.push(num)
+
+    console.log(answer)
+
+}
+
+
+
+
